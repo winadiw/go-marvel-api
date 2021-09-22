fa784cfc140cf29141105c772e4146b204a60081
Winadi Wiratama <winadi.wiratama@dana.id>
2021-09-22 22:35:21 +0700

    Use cache from redis

--- cache/cache.go
+++ cache/cache.go
@@ -12,12 +12,29 @@ var RDB *redis.Client
 
 var ctx = context.Background()
 
-func GetForKey(key string) (string, error) {
-	val, err := RDB.Get(ctx, key).Result()
-	return val, err
+// RedisCaching implements fiber.Storage
+type RedisCaching struct {
 }
 
-func SetForKey(key string, data string, expiry time.Duration) error {
-	err := RDB.Set(ctx, key, data, expiry).Err()
-	return err
+func (rc RedisCaching) Get(key string) ([]byte, error) {
+	return RDB.Get(ctx, key).Bytes()
+}
+
+func (rc RedisCaching) Set(key string, val []byte, ttl time.Duration) error {
+	return RDB.Set(ctx, key, val, ttl).Err()
+}
+
+func (rc RedisCaching) Delete(key string) error {
+	RDB.Del(ctx, key)
+	return nil
+}
+
+func (rc RedisCaching) Reset() error {
+	RDB.FlushAll(ctx)
+	return nil
+}
+
+func (rc RedisCaching) Close() error {
+	RDB.Close()
+	return nil
 }
--- router/router-default.go
+++ router/router-default.go
@@ -1,9 +1,13 @@
 package router
 
 import (
+	"time"
+
 	"github.com/gofiber/fiber/v2"
+	fibercache "github.com/gofiber/fiber/v2/middleware/cache"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/requestid"
+	"github.com/winadiw/go-marvel-api/cache"
 	"github.com/winadiw/go-marvel-api/handler"
 	"github.com/winadiw/go-marvel-api/utils"
 )
@@ -12,6 +16,14 @@ import (
 func SetupRoutes(app *fiber.App) {
 	// Middleware
 	app.Use(requestid.New())
+	app.Use(fibercache.New(fibercache.Config{
+		Next: func(c *fiber.Ctx) bool {
+			return c.Query("refresh") == "true"
+		},
+		Expiration:   30 * time.Minute,
+		CacheControl: true,
+		Storage:      cache.RedisCaching{},
+	}))
 	api := app.Group("/api", logger.New(logger.Config{
 		Format:     utils.LoggerFormat(),
 		TimeFormat: "2006-01-02T15:04:05Z07:00",

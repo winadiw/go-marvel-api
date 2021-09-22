d150a999925f872e78769f208b26b9f445c4ecb1
Winadi Wiratama <winadi.wiratama@dana.id>
2021-09-22 20:30:12 +0700

    Clean up codes

--- external/marvel.go
+++ external/marvel.go
@@ -12,6 +12,11 @@ import (
 	"github.com/winadiw/go-marvel-api/config"
 )
 
+var baseUrl = config.Config("MARVEL_BASE_URL")
+var publicKey = config.Config("MARVEL_PUBLIC_KEY")
+var privateKey = config.Config("MARVEL_PRIVATE_KEY")
+
+// getMD5Hash returns md5 of a given string
 func getMD5Hash(text string) string {
 	hash := md5.Sum([]byte(text))
 	return hex.EncodeToString(hash[:])
@@ -19,21 +24,13 @@ func getMD5Hash(text string) string {
 
 // getAuthenticatedUrl returns ready to use URL to call Marvel API
 func getAuthenticatedUrl(url string) string {
-	now := time.Now() // current local time
-	ts := now.Unix()
-
-	baseUrl := config.Config("MARVEL_BASE_URL")
-	publicKey := config.Config("MARVEL_PUBLIC_KEY")
-	privateKey := config.Config("MARVEL_PRIVATE_KEY")
-
+	ts := time.Now().Unix()
 	hash := getMD5Hash(fmt.Sprintf("%d%s%s", ts, privateKey, publicKey))
-
 	authUrl := fmt.Sprintf("%s/%s?ts=%d&apikey=%s&hash=%s", baseUrl, url, ts, publicKey, hash)
-	fmt.Println(authUrl)
-
 	return authUrl
 }
 
+// MarvelGetCharactersById returns character by ID
 func MarvelGetCharactersById(ID string) error {
 	response, err := http.Get(getAuthenticatedUrl("/v1/public/characters/" + ID))
 
